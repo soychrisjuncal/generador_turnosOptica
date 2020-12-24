b7b89cc25ee97da70b11ce74d739c43bb025a9e5
soychrisjuncal <juncalcf@gmail.com>
2020-12-23 21:30:09 -0300

    le agrege unas animaciones con js a todos los bloques de texto y mejore el aspecto de los botones

---
+++ js/index.js
@@ -0,0 +1,93 @@
+window.sr = ScrollReveal();
+
+	sr.reveal('.contenido__bloqueNovedades1', {
+
+		duration: 3000,
+		origin: 'right',
+		distance: '-100px'
+
+
+	})
+
+
+	sr.reveal('.contenido__bloqueNovedades2', {
+
+		duration: 3000,
+		origin: 'left',
+		distance: '-100px'
+
+
+	})
+
+
+	sr.reveal('.contenido__bloqueHome', {
+
+		duration: 2500,
+		origin: 'right',
+		distance: '-100px'
+
+
+	})
+
+		sr.reveal('.contenido__bloqueTurnos', {
+
+		duration: 2500,
+		origin: 'right',
+		distance: '-100px'
+
+
+	})
+
+		sr.reveal('.contenido__padreFlexForm', {
+
+		duration: 2500,
+		origin: 'left',
+		distance: '-100px'
+
+
+	})
+
+		sr.reveal('.contenido__bloqueSalud', {
+
+		duration: 2500,
+		origin: 'left',
+		distance: '-100px'
+
+
+	})
+
+		sr.reveal('.contenido__bloqueAyuda1', {
+
+		duration: 2500,
+		origin: 'right',
+		distance: '-100px'
+
+
+	})
+
+		sr.reveal('.contenido__bloqueAyuda2', {
+
+		duration: 2500,
+		origin: 'left',
+		distance: '-100px'
+
+
+	})
+
+		sr.reveal('.contenido__bloqueContacto1', {
+
+		duration: 2500,
+		origin: 'right',
+		distance: '-100px'
+
+
+	})
+
+		sr.reveal('.contenido__formContacto', {
+
+		duration: 2500,
+		origin: 'left',
+		distance: '-100px'
+
+
+	})
